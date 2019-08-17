5aceb18c0e7a9efb1fa4638caaf81a9b64e43bf8
JamesB <jblazer1@yahoo.com>
2019-08-17 11:42:54 -0700

    Add Routes and NavLinks

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import { Route, NavLink } from "react-router-dom";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList.js";
+import WelcomePage from "./components/WelcomePage.js";
+import LocationList from "./components/LocationsList.js";
+// import SearchForm from "./components/SearchForm.js";
+
+import "./App.css";
+
 // import AppRouter from "./components/AppRouter.js";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <nav>
+        <NavLink exact to="/">
+          Home Page
+        </NavLink>
+        <NavLink to="/characters">Characters</NavLink>
+        <NavLink to="/locations">Locations</NavLink>
+        <NavLink to="/episodes">Episodes</NavLink>
+      </nav>
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
+      <Route path="/locations" component={LocationList} />
+      {/* <Route path="/episodes" component={SearchForm} /> */}
+
       {/* <TabNav /> */}
       {/* <AppRouter /> */}
     </main>
