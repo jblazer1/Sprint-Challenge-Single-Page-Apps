658f509fe577376da1449818d533a4e162d6cad8
JamesB <jblazer1@yahoo.com>
2019-08-17 17:25:20 -0700

    Character page has image and name - working on Location page

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 export default function CharacterCard(props) {
+  // console.log(props);
   return (
-    <div>
+    <div className="char-card">
       {/* <span>todo: character</span> */}
+      {/* {props.character.map(info => (
+        <img src={info.url} />
+      ))} */}
+      <img src={props.character.image} />
       {props.character.name}
     </div>
   );
